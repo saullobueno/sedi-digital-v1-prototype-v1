3589cc2b1bc9711688e5ec657e45367f205a8a44
Saullo Bueno <saullobueno@gmail.com>
2025-11-14 01:56:54 +0000

    Modal principal de edicao

--- src/contexts/color-mode/index.tsx
+++ src/contexts/color-mode/index.tsx
@@ -1,57 +1,59 @@
 import { RefineThemes } from "@refinedev/antd";
+import locale from 'antd/locale/pt_BR';
 import { ConfigProvider, theme } from "antd";
 import {
-  createContext,
-  useEffect,
-  useState,
-  type PropsWithChildren,
+	createContext,
+	useEffect,
+	useState,
+	type PropsWithChildren,
 } from "react";
 
 type ColorModeContextType = {
-  mode: string;
-  setMode: (mode: string) => void;
+	mode: string;
+	setMode: (mode: string) => void;
 };
 
 export const ColorModeContext = createContext<ColorModeContextType>(
-  {} as ColorModeContextType
+	{} as ColorModeContextType
 );
 
 export const ColorModeContextProvider: React.FC<PropsWithChildren> = ({
-  children,
+	children,
 }) => {
-  const colorModeFromLocalStorage = localStorage.getItem("colorMode");
-  const isSystemPreferenceDark = window?.matchMedia(
-    "(prefers-color-scheme: light)"
-  ).matches;
+	const colorModeFromLocalStorage = localStorage.getItem("colorMode");
+	const isSystemPreferenceDark = window?.matchMedia(
+		"(prefers-color-scheme: light)"
+	).matches;
 
-  const systemPreference = isSystemPreferenceDark ? "dark" : "light";
-  const [mode, setMode] = useState("light"
-    /* colorModeFromLocalStorage || systemPreference */
-  );
+	const systemPreference = isSystemPreferenceDark ? "dark" : "light";
+	const [mode, setMode] = useState("light"
+		/* colorModeFromLocalStorage || systemPreference */
+	);
 
-  useEffect(() => {
-    window.localStorage.setItem("colorMode", mode);
-  }, [mode]);
+	useEffect(() => {
+		window.localStorage.setItem("colorMode", mode);
+	}, [mode]);
 
-  const setColorMode = () => {
-    if (mode === "light") {
-      setMode("dark");
-    } else {
-      setMode("light");
-    }
-  };
+	const setColorMode = () => {
+		if (mode === "light") {
+			setMode("dark");
+		} else {
+			setMode("light");
+		}
+	};
 
-  const { darkAlgorithm, defaultAlgorithm } = theme;
+	const { darkAlgorithm, defaultAlgorithm } = theme;
 
-  return (
-    <ColorModeContext.Provider
-      value={{
-        setMode: setColorMode,
-        mode,
-      }}
-    >
-      <ConfigProvider
-        // you can change the theme colors here. example: ...RefineThemes.Magenta,
+	return (
+		<ColorModeContext.Provider
+			value={{
+				setMode: setColorMode,
+				mode,
+			}}
+		>
+			<ConfigProvider
+				// you can change the theme colors here. example: ...RefineThemes.Magenta,
+				locale={locale}
 				theme={{
 					...RefineThemes.Blue,
 					algorithm: mode === "dark" ? darkAlgorithm : defaultAlgorithm,
@@ -71,10 +73,10 @@ export const ColorModeContextProvider: React.FC<PropsWithChildren> = ({
 							/* fontSize: 16, */
 						}
 					}
-        }}
-      >
-        {children}
-      </ConfigProvider>
-    </ColorModeContext.Provider>
-  );
+				}}
+			>
+				{children}
+			</ConfigProvider>
+		</ColorModeContext.Provider>
+	);
 };
